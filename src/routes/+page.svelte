6491a157f4683d6b17cfe6a61f7b9ab52d3cae8a
YayBlaze <138394031+YayBlaze@users.noreply.github.com>
2023-11-16 18:27:25 -0800

    feat: pushed changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,147 +1,143 @@
-<title>Blaze's todo</title>
-
 <script lang="ts">
-    let task = ""
-    let tasks = ["This is a meaningless task"]
-    let comptasks = []
-    let checktask = ""
-    function addtask() {
-        if (task !== ""){
-            tasks.push(task)
-            tasks = tasks
-        }
-        task = ""
+  let task = "";
+  let tasks = ["This is a meaningless task"];
+  let comptasks = [];
+  let checktask = [];
+  function addtask() {
+    if (task !== "") {
+      tasks.push(task);
+      tasks = tasks;
     }
-    function removetask() {
-        comptasks.push(checktask)
-        comptasks = comptasks
-        let idx = tasks.indexOf(checktask)
-        console.log(idx)
-        tasks.splice(idx, 1)
-        tasks = tasks
-        checktask = ""
+    task = "";
+  }
+  function removetask() {
+    comptasks.push(checktask);
+    comptasks = comptasks;
+    // let idx = tasks.indexOf(checktask);
+    // console.log(idx);
+    // tasks.splice(idx, 1);
+    // tasks = tasks;
+    // checktask = "";
+    for (var i = 0; i < checktask.length; i++) {
+      tasks.splice(tasks.indexOf(checktask[i]), 1);
     }
-//    if (comptasks.length == 0) {comptasks = JSON.parse(localStorage.getItem("comp")) || ""};
-//    if (tasks.length == 0) {tasks = JSON.parse(localStorage.getItem("tasks")) || ""}
-//
-//    $: localStorage.setItem("comp",JSON.stringify(comptasks));
-//    $: localStorage.setItem("tasks",JSON.stringify(tasks));
+    checktask = [];
+  }
+  //    if (comptasks.length == 0) {comptasks = JSON.parse(localStorage.getItem("comp")) || ""};
+  //    if (tasks.length == 0) {tasks = JSON.parse(localStorage.getItem("tasks")) || ""}
+  //
+  //    $: localStorage.setItem("comp",JSON.stringify(comptasks));
+  //    $: localStorage.setItem("tasks",JSON.stringify(tasks));
 </script>
 
+<title>Blaze's todo</title>
 
 <body class="body">
-    <h1 class="rainbow">Blaze's goofy ahh todo list</h1>
-    <h2 class="rainbow">Add a Task:</h2>
-    <h3 id="input-text">Task Name:</h3> 
-    <input type="text-box" style="color: cornflowerblue;" bind:value={task}>
-    <button id="update" on:click="{addtask}">Add Task</button>
-    <h2 class="rainbow">Tasks todo:</h2>
-    {#each tasks as task, i}
-        <div class="todotask">
-            <p style="color: cornflowerblue;">Task {i + 1}: {task}</p>
-            <input 
-                type="checkbox" 
-                value={task}
-                bind:group={checktask}
-            />
-            <p>{checktask}</p>
-        </div>
+  <h1 class="rainbow">Blaze's goofy ahh todo list</h1>
+  <h2 class="rainbow">Add a Task:</h2>
+  <h3 id="input-text">Task Name:</h3>
+  <input type="text-box" style="color: cornflowerblue;" bind:value={task} />
+  <button id="update" on:click={addtask}>Add Task</button>
+  <h2 class="rainbow">Tasks todo:</h2>
+  {#each tasks as task, i}
+    <div class="todotask">
+      <p style="color: cornflowerblue;">Task {i + 1}: {task}</p>
+      <input type="checkbox" value={task} bind:group={checktask} />
+      <!-- <p style="color: cornflowerblue;">{checktask[0]}</p> -->
+    </div>
+  {/each}
+  <button id="removetask" on:click={removetask}
+    >Remove all selected tasks</button
+  >
+
+  <h2 class="rainbow">Completed Tasks:</h2>
+  {#if comptasks.length != 0}
+    {#each comptasks as donetask}
+      <p style="color: cornflowerblue">{donetask} is now complete! Good Job!</p>
     {/each}
-    <button id="removetask" on:click="{removetask}">Remove all selected tasks</button>
-
-
-    <h2 class="rainbow">Completed Tasks:</h2>
-    {#if comptasks.length != 0}
-        {#each comptasks as donetask}
-            <p style="color: cornflowerblue">{donetask} is now complete! Good Job!</p>
-        {/each}
-    {:else}
-        <p style="color: cornflowerblue;">lol no completed tasks</p>
-    {/if}
+  {:else}
+    <p style="color: cornflowerblue;">lol no completed tasks</p>
+  {/if}
 </body>
 
-
-
-
-
-
 <style>
-    .rainbow {
-        background: linear-gradient(
-            45deg,
-            #ff0000,
-            #ff7f00,
-            #ffff00,
-            #00ff00,
-            #0000ff,
-            #4b0082,
-            #8b00ff
-        );
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        background-size: 400% 400%;
-        -webkit-animation: rainbowanim 10s ease infinite;
-        -moz-animation: rainbowanim 10s ease infinite;
-        animation: rainbowanim 10s ease infinite;
-    }
+  .rainbow {
+    background: linear-gradient(
+      45deg,
+      #ff0000,
+      #ff7f00,
+      #ffff00,
+      #00ff00,
+      #0000ff,
+      #4b0082,
+      #8b00ff
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-size: 400% 400%;
+    -webkit-animation: rainbowanim 10s ease infinite;
+    -moz-animation: rainbowanim 10s ease infinite;
+    animation: rainbowanim 10s ease infinite;
+  }
 
-    @-webkit-keyframes rainbowanim {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
+  @-webkit-keyframes rainbowanim {
+    0% {
+      background-position: 0% 50%;
     }
-    @-moz-keyframes rainbowanim {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
+    50% {
+      background-position: 100% 50%;
     }
-    @keyframes rainbowanim {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
+    100% {
+      background-position: 0% 50%;
     }
-    #inputtext {
-        color: cornflowerblue;
+  }
+  @-moz-keyframes rainbowanim {
+    0% {
+      background-position: 0% 50%;
     }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+  @keyframes rainbowanim {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+  #inputtext {
+    color: cornflowerblue;
+  }
 
-    #update {
-        color:white;
-        background-color: cornflowerblue;
-        border-style: hidden;
-        font-size: large;
-        width: fit-content;
-    }
+  #update {
+    color: white;
+    background-color: cornflowerblue;
+    border-style: hidden;
+    font-size: large;
+    width: fit-content;
+  }
 
-    .body {
-        background-color: black;
-        width: fit-content;
-    }
+  .body {
+    background-color: black;
+    width: fit-content;
+  }
 
-    .todotask {
-        display: flex;
-    }
+  .todotask {
+    display: flex;
+  }
 
-    #removetask {
-        color:white;
-        background-color: cornflowerblue;
-        border-style: hidden;
-    }
+  #removetask {
+    color: white;
+    background-color: cornflowerblue;
+    border-style: hidden;
+  }
 </style>
+
